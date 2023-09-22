dad9fdfa969a2d368ffd990f63ea848d858b7968
ktkim <ktkim0111@gmail.com>
2023-09-22 18:44:11 +0900

    error, 저장 중 추가

--- src/components/upgr-form.jsx
+++ src/components/upgr-form.jsx
@@ -13,9 +13,11 @@ const InputWrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 5px;
+  width: 100%;
 `;
 
 const Input = styled.input`
+  width: 100%;
   padding: 10px 20px;
   border: 2px solid white;
   border-radius: 10px;
@@ -35,11 +37,21 @@ const Input = styled.input`
   }
 `;
 
+const Error = styled.div`
+  margin-top: 1vh;
+  font-size: 18px;
+  font-weight: 600;
+  color: tomato;
+  text-align: center;
+`;
+
 export default function UPGRForm() {
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [first, setFirst] = useState("");
   const [last, setLast] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   const onChange = (e) => {
     const {
@@ -58,7 +70,9 @@ export default function UPGRForm() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
+    setError("");
     try {
+      setLoading(true);
       await addDoc(collection(db, "upgr-form"), {
         name,
         address,
@@ -66,49 +80,59 @@ export default function UPGRForm() {
         last,
         createAt: Date.now(),
       });
+      alert("저장되었습니다.");
     } catch (e) {
-      console.log(e);
+      setError(e.message);
+    } finally {
+      setLoading(false);
+      setName("");
+      setAddress("");
+      setFirst("");
+      setLast("");
     }
   };
 
   return (
-    <Form onSubmit={onSubmit}>
-      <Input
-        name="name"
-        onChange={onChange}
-        type="text"
-        value={name}
-        placeholder="이름"
-        required
-      />
-      <InputWrapper>
+    <>
+      <Form onSubmit={onSubmit}>
         <Input
-          name="first"
+          name="name"
           onChange={onChange}
           type="text"
-          value={first}
-          placeholder="주민등록번호 앞자리"
+          value={name}
+          placeholder="이름"
           required
         />
-        -
+        <InputWrapper>
+          <Input
+            name="first"
+            onChange={onChange}
+            type="text"
+            value={first}
+            placeholder="주민등록번호 앞자리"
+            required
+          />
+          -
+          <Input
+            name="last"
+            onChange={onChange}
+            type="text"
+            value={last}
+            placeholder="주민등록번호 뒷자리"
+            required
+          />
+        </InputWrapper>
         <Input
-          name="last"
+          name="address"
           onChange={onChange}
-          type="text"
-          value={last}
-          placeholder="주민등록번호 뒷자리"
+          type="address"
+          value={address}
+          placeholder="주소 - 동까지"
           required
         />
-      </InputWrapper>
-      <Input
-        name="address"
-        onChange={onChange}
-        type="address"
-        value={address}
-        placeholder="주소 - 동까지"
-        required
-      />
-      <Input type="submit" value="제출하기" />
-    </Form>
+        <Input type="submit" value={isLoading ? "저장 중" : "제출하기"} />
+      </Form>
+      <Error>{error}</Error>
+    </>
   );
 }
